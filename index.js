04723580e7a14065e71ed694cea22de702561559
Robyn Paulsen <149692745+Robyn011@users.noreply.github.com>
2024-05-08 13:38:20 +0200

    Completed my "if else" loop

--- index.js
+++ index.js
@@ -6,31 +6,57 @@ const time = 3600; // seconds (1 hour)
 const distance = 0; // distance (km)
 const fuel = 5000; // remaining fuel (kg)
 const fuelBurnRate = 0.5; // fuel burn rate (kg/s)
-
+var newDistance;
+var remainingFuel;
+var newVelocity;
 
 //calculates the final/new velocity
 function calcNewVel() { 
-  return velocity  + ((acceleration*3.6) * time)
-}
+  newVelocity = velocity  + ((acceleration*3.6) * time);
+  
+  if (newVelocity != 48880 ) 
+    {
+      throw new Error (`${newVelocity} km/h is not equal to 48880km/h.`);
+    }
+    else 
+    {
+     console.log(`Corrected New Velocity:${newVelocity} km/h.`);
+    }
+  
+};
+calcNewVel()
 
 //calculates the final/ new velocity
 function calcNewDist() { 
-  return distance + (velocity*(time/60/60)) 
-}
+ newDistance= distance + (velocity*(time/60/60)) ;
+ if (newDistance != 10000 ) 
+  {
+    throw new Error (`${newDistance} km is not equal to 10000km.`);
+  }
+  else 
+  {
+   console.log(`Corrected New Distance: ${newDistance} km.`);
+  }
+
+};
+
+calcNewDist()
 
 //calculates the final/ new velocity
 function calcRemainingFuel() { 
-  return fuel - (fuelBurnRate*time)
-}
-var newDistance = calcNewDist() ;
-var remainingFuel = calcRemainingFuel() ;
-var newVelocity = calcNewVel() ;
+  remainingFuel =fuel - (fuelBurnRate*time);
+  if (remainingFuel != 3200 ) 
+    {
+     throw new Error (`${remainingFuel} kg is not equal to 3200kg.`);
+    }
+    else 
+    {
+     console.log(`Corrected Remaining Fuel: ${remainingFuel} kg.`);
 
-console.log(`Approximately: ${newVelocity} km/h after correction.`);
-console.log(`Approximately: ${newDistance} km after correction.`);
-console.log(`Approximately: ${remainingFuel} kg after correction.`);
-
-
+    }
+ 
+};
+calcRemainingFuel()
 
 
 
